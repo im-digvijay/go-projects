9ed6469a9842eb1b4d3e68a3f5c939155d92f828
Digvijay Singh <singhdigvijay0007@gmail.com>
2023-11-29 16:01:09 +0530

    Add: bookstore crud api with mysql

---
+++ go-bookstore/pkg/config/app.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	dsn := "test:test@(localhost:3306)/book_store?charset=utf8mb4&parseTime=True&loc=Local"
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
